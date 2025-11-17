dde1507079e9827ccf49328e76f6d325c12c1e48
jansilgasoc@gmail.com <jansilgasoc859@gmail.com>
2025-11-17 16:17:58 +0800

    first commit

--- server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const app = express();
-
-app.use(express.static("public")); // folder with your HTML files
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server running");
-});
